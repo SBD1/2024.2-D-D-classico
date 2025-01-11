8797729bdb13b97070b9ccebda07ebf02b93cc14
caiolamego <caio.lamego04@gmail.com>
2025-01-11 15:31:45 -0300

    Adiciona tabelas ao BD

--- src/sql/DDL.sql
+++ src/sql/DDL.sql
@@ -36,7 +36,14 @@ CREATE TABLE IF NOT EXISTS Personagem (
     tipo_personagem char(1) NOT NULL,
     vida int NOT NULL,
     nivel int NOT NULL,
-    xp_base int NOT NULL DEFAULT 0
+    xp_base int NOT NULL DEFAULT 0,
+    destreza int NOT NULL,
+    carisma int NOT NULL,
+    forca int NOT NULL,
+    constituicao int NOT NULL,
+    sabedoria int NOT NULL,
+    inteligencia int NOT NULL,
+     
 );
 
 CREATE TABLE IF NOT EXISTS Mundo (
@@ -62,6 +69,27 @@ CREATE TABLE IF NOT EXISTS Inventario (
     capacidade int NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS Venda (
+    id SERIAL PRIMARY KEY,
+    id_loja int NOT NULL,
+    id_instancia_item int NOT NULL,
+    preco int NOT NULL,
+    quantidade int NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS Inst_Item (
+    id SERIAL PRIMARY KEY,
+    id_item int NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS Missao (
+    id SERIAL PRIMARY KEY,
+    recompensa int NOT NULL,
+    id_personagem int NOT NULL,
+    titulo char(100) NOT NULL,
+    objetivo char(500) NOT NULL
+)
+
 
 -- Keys
 
@@ -74,5 +102,10 @@ ALTER TABLE Regiao ADD CONSTRAINT "FK_06" PRIMARY KEY (id_mundo) REFERENCES Mund
 ALTER TABLE Personagem ADD CONSTRAINT "FK_07" FOREIGN KEY (id_classe) REFERENCES Classe (id);
 ALTER TABLE Loja ADD CONSTRAINT "FK_08" FOREIGN KEY (dono) REFERENCES Personagem (id);
 ALTER TABLE Inventario ADD CONSTRAINT "FK_09" FOREIGN KEY (id_pc) REFERENCES Personagem (id);
+ALTER TABLE Venda ADD CONSTRAINT "FK_10" FOREIGN KEY (id_loja) REFERENCES Loja (id);
+ALTER TABLE Venda ADD CONSTRAINT "FK_11" FOREIGN KEY (id_instancia_item) REFERENCES Inst_Item (id);
+ALTER TABLE Inst_Item ADD CONSTRAINT "FK_12" FOREIGN KEY (id_item) REFERENCES Item (id);
+ALTER TABLE Missao ADD CONSTRAINT "FK_13" FOREIGN KEY (recompensa) REFERENCES Inst_Item (id);
+ALTER TABLE Missao ADD CONSTRAINT "FK_14" FOREIGN KEY (id_personagem) REFERENCES Personagem (id);
 
 
